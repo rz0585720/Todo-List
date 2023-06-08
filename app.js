ff18d15a08455958e4d1dcab59153e36992076a7
rz0585720 <cherish05061231@gmail.com>
2023-06-09 00:28:24 +0800

    add mongoose & dotenv

--- app.js
+++ app.js
@@ -2,6 +2,27 @@ const express = require('express')
 const app = express()
 const exphbs = require('express-handlebars')
 const port = 3000
+const mongoose = require('mongoose') // 載入 mongoose
+
+if (process.env.NODE_ENV !== 'production') {
+	require('dotenv').config()
+}
+
+mongoose.connect(process.env.MONGODB_URI, {
+	useNewUrlParser: true,
+	useUnifiedTopology: true,
+}) // 設定連線到 mongoDB
+
+// 取得資料庫連線狀態
+const db = mongoose.connection
+// 連線異常
+db.on('error', () => {
+	console.log('mongodb error!')
+})
+// 連線成功
+db.once('open', () => {
+	console.log('mongodb connected!')
+})
 
 // template engine
 app.engine('handlebars', exphbs({ defaultLayout: 'main' }))
