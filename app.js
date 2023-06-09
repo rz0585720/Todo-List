005743a28da8b71b214b2a997da6372b2ee19809
rz0585720 <cherish05061231@gmail.com>
2023-06-09 20:31:49 +0800

    add detail page

--- app.js
+++ app.js
@@ -37,8 +37,8 @@ app.use(bodyParser.urlencoded({ extended: true }))
 app.get('/', (req, res) => {
 	Todo.find() // 取出 Todo model 裡的所有資料
 		.lean() // 把 Mongoose 的 Model 物件轉換成乾淨的 JavaScript 資料陣列
-		.then((todos) => res.render('index', { todos })) // 將資料傳給 index 樣板
-		.catch((error) => console.error(error)) // 錯誤處理
+		.then(todos => res.render('index', { todos })) // 將資料傳給 index 樣板
+		.catch(error => console.error(error)) // 錯誤處理
 })
 
 app.get('/todos/new', (req, res) => {
@@ -49,7 +49,15 @@ app.post('/todos', (req, res) => {
 	const name = req.body.name // 從 req.body 拿出表單裡的 name 資料
 	return Todo.create({ name }) // 存入資料庫
 		.then(() => res.redirect('/')) // 新增完成後導回首頁
-		.catch((error) => console.log(error))
+		.catch(error => console.log(error))
+})
+
+app.get('/todos/:id', (req, res) => {
+	const id = req.params.id
+	return Todo.findById(id)
+		.lean()
+		.then(todo => res.render('detail', { todo }))
+		.catch(error => console.log(error))
 })
 
 // start server
