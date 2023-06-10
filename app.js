e54478e4f212e5eb7140748a3b533d2c680cb2f7
rz0585720 <cherish05061231@gmail.com>
2023-06-10 22:43:40 +0800

    refactor mongoose config

--- app.js
+++ app.js
@@ -1,33 +1,13 @@
 const express = require('express')
-const app = express()
-const port = 3000
 const exphbs = require('express-handlebars')
-const mongoose = require('mongoose') 
 const bodyParser = require('body-parser')
-const Todo = require('./models/todo') 
 const methodOverride = require('method-override')
+
 const routes = require('./routes')
+require('./config/mongoose')
 
-if (process.env.NODE_ENV !== 'production') {
-	require('dotenv').config()
-}
-
-// 設定連線到 mongoDB
-mongoose.connect(process.env.MONGODB_URI, {
-	useNewUrlParser: true,
-	useUnifiedTopology: true,
-})
-
-// 取得資料庫連線狀態
-const db = mongoose.connection
-// 連線異常
-db.on('error', () => {
-	console.log('mongodb error!')
-})
-// 連線成功
-db.once('open', () => {
-	console.log('mongodb connected!')
-})
+const port = 3000
+const app = express()
 
 // template engine
 app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }))
--- models/seeds/todoSeeder.js
+++ models/seeds/todoSeeder.js
@@ -1,28 +1,9 @@
-const mongoose = require('mongoose')
+const db = require('../../config/mongoose')
 const Todo = require('../todo')
 
-if (process.env.NODE_ENV !== 'production') {
-	require('dotenv').config()
-}
-
-mongoose.connect(process.env.MONGODB_URI, {
-	useNewUrlParser: true,
-	useUnifiedTopology: true,
-})
-
-// 取得資料庫連線狀態
-const db = mongoose.connection
-// 連線異常
-db.on('error', () => {
-	console.log('mongodb error!')
-})
-// 連線成功
 db.once('open', () => {
-	console.log('mongodb connected!')
-
 	for (let i = 0; i < 10; i++) {
 		Todo.create({ name: `name-${i}` })
 	}
-
 	console.log('done!')
 })
