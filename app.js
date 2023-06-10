4ac9f7599104e6ad6a1ab5abfe6b0ff8d63d7712
rz0585720 <cherish05061231@gmail.com>
2023-06-10 22:30:46 +0800

    refactor router

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ const mongoose = require('mongoose') // 載入 mongoose
 const bodyParser = require('body-parser')
 const Todo = require('./models/todo') // 載入 Todo model
 const methodOverride = require('method-override')
+const routes = require('./routes')
 
 if (process.env.NODE_ENV !== 'production') {
 	require('dotenv').config()
@@ -34,63 +35,11 @@ app.set('view engine', 'hbs')
 
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(methodOverride('_method'))
+app.use(routes)
 
 // routes settings
-app.get('/', (req, res) => {
-	Todo.find() // 取出 Todo model 裡的所有資料
-		.lean() // 把 Mongoose 的 Model 物件轉換成乾淨的 JavaScript 資料陣列
-		.sort({ _id: 'asc' }) // desc→降冪, asc→升冪
-		.then(todos => res.render('index', { todos })) // 將資料傳給 index 樣板
-		.catch(error => console.error(error)) // 錯誤處理
-})
 
-app.get('/todos/new', (req, res) => {
-	return res.render('new')
-})
 
-app.post('/todos', (req, res) => {
-	const name = req.body.name // 從 req.body 拿出表單裡的 name 資料
-	return Todo.create({ name }) // 存入資料庫
-		.then(() => res.redirect('/')) // 新增完成後導回首頁
-		.catch(error => console.log(error))
-})
-
-app.get('/todos/:id', (req, res) => {
-	const id = req.params.id
-	return Todo.findById(id)
-		.lean()
-		.then(todo => res.render('detail', { todo }))
-		.catch(error => console.log(error))
-})
-
-app.get('/todos/:id/edit', (req, res) => {
-	const id = req.params.id
-	return Todo.findById(id)
-		.lean()
-		.then(todo => res.render('edit', { todo }))
-		.catch(error => console.log(error))
-})
-
-app.put('/todos/:id', (req, res) => {
-	const id = req.params.id
-	const { name, isDone } = req.body
-	return Todo.findById(id)
-		.then(todo => {
-			todo.name = name
-			todo.isDone = isDone === 'on'
-			return todo.save()
-		})
-		.then(() => res.redirect(`/todos/${id}`))
-		.catch(error => console.log(error))
-})
-
-app.delete('/todos/:id', (req, res) => {
-	const id = req.params.id
-	return Todo.findById(id)
-		.then(todo => todo.remove())
-		.then(() => res.redirect('/'))
-		.catch(error => console.log(error))
-})
 
 // start server
 app.listen(port, () => {
