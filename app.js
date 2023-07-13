0ef73ef5d5aaf25ccca0e580176a9414630d4550
rz0585720 <cherish05061231@gmail.com>
2023-07-13 21:15:43 +0800

    add user login feature

--- app.js
+++ app.js
@@ -1,10 +1,11 @@
 const express = require('express')
+const session = require('express-session')
 const exphbs = require('express-handlebars')
 const bodyParser = require('body-parser')
 const methodOverride = require('method-override')
-const session = require('express-session')
 
 const routes = require('./routes')
+const usePassport = require('./config/passport')
 require('./config/mongoose')
 
 const port = 3000
@@ -14,13 +15,18 @@ const app = express()
 app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }))
 app.set('view engine', 'hbs')
 
-app.use(session({
-	secret: 'ThisIsMySecret',
-	resave: false,
-	saveUninitialized: true
-}))
+app.use(
+	session({
+		secret: 'ThisIsMySecret',
+		resave: false,
+		saveUninitialized: true,
+	})
+)
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(methodOverride('_method'))
+
+usePassport(app)
+
 app.use(routes)
 
 // start server
